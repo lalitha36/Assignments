af758362f1f7f59727b526ce6959f77384c83b5b
lalitha <lalithadevi.vegiraju@gmail.com>
2025-01-29 17:51:48 +0000

    Assignment1DataType

--- src/com/assignments/Assignment1DataTypes.java
+++ src/com/assignments/Assignment1DataTypes.java
@@ -3,8 +3,54 @@ package com.assignments;
 public class Assignment1DataTypes {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		float  temperature = 25.5f;
+        
+		boolean custome_rorder = true;
+		
+		String phonenumber = "123-456-7890";
+		
+		float moneyinbankacoount = 1000.50f;
+		
+		String emailaddress = "john.doe@example.com";
+		
+		float locationlatitude = 37.7749f;
+		
+		float locationlongitude = -122.4194f;
+			
+		boolean maritalstatues = true;
+		
+		String occupation = "Softwear Engineer";
+		
+		String colour = "Blue";
+		
+		short currentyear = 2025;
+		
+		int no_of_followers = 1000000;
+		
+		//Can i write this 1,000,000 in String?
+		
+		String nooffollowers = "1,000,000";
+		
+		float movie_rating = 7.5f;
+		
+		char bloodtype = 'A';
+		
+		String Tille_Of_a_Book = "To kill a Mockingbird";
+		
+		short Number_of_Employees = 500;
+		
+		String Time_Of_An_Event = "2.30 PM";
+		
+		String country = "U.S.A";
+		
+		String Eye_Color = "Brown";
+		
+		String brithplace = "New York City";
+		
+		float Distance_between_two_cities = 200.5f;
+			
+		
+		
 	}
 
 }
